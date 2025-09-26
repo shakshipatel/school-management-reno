692569d115201151a0fdf86e82296edd49c55f53
Shakshi Patel <121892732+shakshipatel@users.noreply.github.com>
2025-09-26 23:05:15 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,9 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'School Management RENO',
+  description: 'School Management RENO',
+  generator: 'School Management RENO',
 }
 
 export default function RootLayout({
